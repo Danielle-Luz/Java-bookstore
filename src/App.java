67fd0cea1f6a3dc3d7f9c1fd8e4eadb1c1e468b4
Danielle Luz <daniln2209@gmail.com>
2024-05-07 21:45:17 -0300

    feat: add main and show menu methods

---
+++ src/App.java
@@ -0,0 +1,61 @@
+import enums.UserType;
+import models.User;
+import services.BookBorrowingService;
+import services.BookService;
+import services.UserService;
+
+public class App {
+
+  public static void main(String[] args) throws Exception {
+    System.out.println(
+      "Welcome to the bookstore!\nYou need to login to continue:"
+    );
+
+    User loggedUser = UserService.login();
+
+    showMenu(loggedUser);
+  }
+
+  public static void showMenu(User loggedUser) {
+    int optionChosen;
+
+    System.out.println("Welcome " + loggedUser.getUsername() + "!");
+
+    while (true) {
+      System.out.println("Select an option:");
+      System.out.println("1 - See books");
+
+      if (loggedUser.getType() == UserType.EMPLOYEE) {
+        System.out.println("2 - Create user");
+      }
+
+      System.out.println("3 - Borrow or return books");
+      System.out.println("4 - Exit");
+
+      try {
+        optionChosen = Integer.parseInt(System.console().readLine());
+      } catch (NumberFormatException e) {
+        System.out.println("You need to type a number value");
+        System.out.println(
+          "----------------------------------------------------"
+        );
+        continue;
+      }
+
+      switch (optionChosen) {
+        case 1 -> BookService.showOptions(loggedUser);
+        case 2 -> {
+            if (loggedUser.getType() == UserType.EMPLOYEE) {
+                UserService.createUser();
+            } }
+        case 3 -> BookBorrowingService.showOptions();
+        case 4 -> System.exit(0);
+        default -> {
+            System.out.println("Select an option between 1 and 4");
+            System.out.println(
+                    "----------------------------------------------------"
+            );}
+      }
+    }
+  }
+}
